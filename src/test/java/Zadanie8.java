4b056af31e2570bc6f615453caea2d1bdff560df
AleksanderSm <aleksander.smardzewski@gmail.com>
2022-12-08 20:34:34 +0100

    Selenium - ćwiczenie kolejne

---
+++ src/test/java/Zadanie8.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Zadanie8 {
+
+    private WebDriver driver;
+
+    @Test
+    public void iloscLinkowNaStronie() {
+        driver = new ChromeDriver();
+        driver.navigate().to("https://www.bbc.com/");
+
+    }
+}
